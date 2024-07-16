dc150293c7e0c57b0675dff9dab8c069b9cfcc39
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-16 15:35:20 +0200

    buildable, before standardize

--- _sunamo/SunamoExceptions/_AddedToAllCsproj/CASunamoExceptions.cs
+++ _sunamo/SunamoExceptions/_AddedToAllCsproj/CASunamoExceptions.cs
@@ -1,6 +1,6 @@
-namespace SunamoEmbeddedResources._sunamo.SunamoExceptions._AddedToAllCsproj;
+//namespace SunamoEmbeddedResources._sunamo.SunamoExceptions._AddedToAllCsproj;
 
-internal class CASunamoExceptions
+internal class CA
 {
     internal static bool IsListStringWrappedInArray<T>(List<T> v2)
     {
