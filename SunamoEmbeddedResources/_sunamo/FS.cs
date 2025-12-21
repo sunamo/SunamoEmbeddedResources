9caf77b4caaf33dd0a6073bd7c2be8facd868819
Radek Jancik <radek.jancik@usys.cz>
2025-12-21 20:48:50 +0100

    25.12.21.1 feat: Added net10.0 to TargetFrameworks

--- SunamoEmbeddedResources/_sunamo/FS.cs
+++ SunamoEmbeddedResources/_sunamo/FS.cs
@@ -1,5 +1,3 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
 namespace SunamoEmbeddedResources._sunamo;
 
 //namespace SunamoEmbeddedResources._sunamo.SunamoExceptions._AddedToAllCsproj;
